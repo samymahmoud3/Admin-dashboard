1d10d72abbd99edc3c2bfa56a0c2c7a2f2c1c3df
samymahmoud3 <samymahmoud2532001@gmail.com>
2023-11-03 00:49:59 +0200

    navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,30 @@
+import './navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="logo">
+        <img src="/logo.svg" alt="logo" />
+        <span>lamadmin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="search" className="icon" />
+        <img src="/app.svg" alt="app" className="icon" />
+        <img src="/expand.svg" alt="expand" className="icon" />
+        <div className="notifications">
+          <img src="/notifications.svg" alt="notifications" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+            alt=""
+          />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" alt="settings" className="icon" />
+      </div>
+    </div>
   )
 }
 
