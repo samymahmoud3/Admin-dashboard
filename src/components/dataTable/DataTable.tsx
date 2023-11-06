317ff7a8dadc3adbee7da64720162371e2131b1c
samymahmoud3 <samymahmoud2532001@gmail.com>
2023-11-06 08:24:37 +0200

    users dataGrid

---
+++ src/components/dataTable/DataTable.tsx
@@ -0,0 +1,66 @@
+import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
+import './dataTable.scss';
+import { Link } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+type Props = {
+  columns: GridColDef[],
+  rows: {}[],
+  slug: string,
+}
+
+const DataTable = (props: Props) => {
+
+  const handleDelete = (id: number) => {
+    console.log(id + ' is deleted');
+  };
+
+  const actionColumn: GridColDef = {
+    field: 'action',
+    headerName: 'Action',
+    width: 200,
+    renderCell: (params) =>
+      <div className='action'>
+        <Link to={`/${props.slug}/${params.row.id}`}>
+          <img src="/view.svg" alt="" />
+        </Link>
+        <div className='delete' onClick={() => handleDelete(params.row.id)}>
+          <img src="/delete.svg" alt="" />
+        </div>
+      </div>
+  };
+  
+  return (
+    <div className='dataTable'>
+      {/* <Box sx={{ height: 400, width: '100%' }}> */}
+        <DataGrid
+          className='dataGrid'
+          rows={props.rows}
+          columns={[...props.columns, actionColumn]}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
+            },
+          }}
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+              quickFilterProps: { debounceMs: 500 },
+            }
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+          disableColumnFilter
+          disableDensitySelector
+          disableColumnSelector
+        />
+      {/* </Box> */}
+    </div>
+  );
+}
+
+export default DataTable
